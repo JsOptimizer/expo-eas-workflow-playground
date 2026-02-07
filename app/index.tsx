b1fd6fa2a2df663da9ae3e9f345d4c2dc242c4c8
jsoptimizer <jsoptimizer.pro@gmail.com>
2026-02-07 11:45:29 +0000

    refactor: replace dynamic item grid with a static 'Hello World' text

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,14 @@
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-<SafeAreaView>
-  <ScrollView contentContainerStyle={{height: '100%'}}>
-    <View style={{
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      gap: 14,
-      padding: 10,
-    }}>
-      {
-        Array.from({length: 10}).map((_, index) => (
-       <TouchableOpacity key={index} style={{width:"48%", borderWidth: 1, borderColor: 'red',minHeight:150,borderRadius:20,display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
         <View>
-          <Text>Item {index}</Text>
+          <Text>Hello World</Text>
         </View>
-       </TouchableOpacity>
-        ))
-      }
-    </View>
-  </ScrollView>
-</SafeAreaView>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
